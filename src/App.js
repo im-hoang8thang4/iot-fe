116ca8b46ba7984e52ff6baafc1d03bb0a2bd938
Hoang ne <hoanghai08042001xxx@gmail.com>
2022-10-25 17:03:04 +0700

    modified:   src/index.js 	new file:   tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Switch } from "@material-tailwind/react";
+import axios from "axios";
+import { useState } from "react";
+import bg from "./assets/iot-bg2.jpg";
 function App() {
+  const [toggleLight, setToggleLight] = useState(false);
+  const [autoLight, setAutoLight] = useState(false);
+  const handletoggleLight = async () => {
+    console.log("toggleLight");
+      await axios.patch("http://iot.tuiladat.ml/light/LIGHT_01", {
+      mode: "manual",
+      enabled: !toggleLight,
+    });
+    setToggleLight(!toggleLight);
+  };
+  const handleautoLight = async () => {
+    setAutoLight(!autoLight);
+    await axios.patch("http://iot.tuiladat.ml/light/LIGHT_02", {
+      mode: "auto",
+      enabled: !autoLight,
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen flex flex-col justify-center items-center relative">
+      <img src={bg} className="w-full h-full object-cover" />
+      <div className="flex flex-col absolute gap-3 right-[20%] top-[20%]">
+        <div className="flex gap-2 items-center">
+          <Switch
+            id="toggleLight"
+            ripple={true}
+            checked={toggleLight}
+            onClick={handletoggleLight}
+          />
+          <span className="text-lg font-semibold text-white">
+            Điều khiển bật tắt bóng đèn
+          </span>
+        </div>
+        <div className="flex gap-2 items-center">
+          <Switch
+            id="autoLight"
+            ripple={true}
+            checked={autoLight}
+            onClick={handleautoLight}
+          />
+          <span className="text-lg font-semibold text-white">
+            Tự động bật tắt đèn khi môi trường sáng thay đổi
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
