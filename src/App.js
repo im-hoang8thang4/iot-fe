3a53cf0db9d6c7086fda44e95ab5a5108c6f08c2
Hoang ne <hoanghai08042001xxx@gmail.com>
2022-10-25 18:52:47 +0700

    aaaaa

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import { Switch } from "@material-tailwind/react";
 import axios from "axios";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import bg from "./assets/iot-bg2.jpg";
 function App() {
-  const [toggleLight, setToggleLight] = useState(false);
+  const [light, setLight] = useState([])
   const [autoLight, setAutoLight] = useState(false);
-  const handletoggleLight = async () => {
-    console.log("toggleLight");
+  const getLight = async () =>{
+    const res = await axios.get('http://iot.tuiladat.ml/light')
+    setLight(res.data)
+  }
+  useEffect(()=>{
+    getLight()
+  },[])
+  const handletoggleLight = async (l) => {
+    console.log(l.lightName)
     await axios.patch(
-      "http://iot.tuiladat.ml/light/LIGHT_01",
+      `http://iot.tuiladat.ml/light/${l.lightName}`,
       {
         mode: 1,
-        enabled: !toggleLight,
+        enabled: !l.enabled,
       },
       {
         headers: {
@@ -19,8 +26,20 @@ function App() {
         },
       }
     );
-    setToggleLight(!toggleLight);
+    setLight(light.map(m=>{
+      if(l.lightName === m.lightName){
+        return{
+          ...l, enabled: !l.enabled
+        }
+
+      }
+      else{
+        return m
+      }
+    }))
+   
   };
+  console.log(light)
   const handleautoLight = async () => {
     setAutoLight(!autoLight);
     await axios.patch(
@@ -40,23 +59,27 @@ function App() {
     <div className="w-full h-screen flex flex-col justify-center items-center relative">
       <img src={bg} className="w-full h-full object-cover" />
       <div className="flex flex-col absolute gap-3 right-[20%] top-[20%]">
-        <div className="flex gap-2 items-center">
-          <Switch
-            id="toggleLight"
-            ripple={true}
-            checked={toggleLight}
-            onChange={handletoggleLight}
-          />
-          <span className="text-lg font-semibold text-white">
-            Điều khiển bật tắt bóng đèn
-          </span>
-        </div>
+        {
+          light.map(l =>(
+            <div className="flex gap-2 items-center">
+            <Switch
+              id={l.id}
+              ripple={true}
+              checked={l.enabled}
+              onChange={()=>handletoggleLight(l)}
+            />
+            <span className="text-lg font-semibold text-white">
+             {l.lightName}
+            </span>
+          </div>
+          ))
+        }
         <div className="flex gap-2 items-center">
           <Switch
             id="autoLight"
             ripple={true}
             checked={autoLight}
-            onChange={handleautoLight}
+            onClick={handleautoLight}
           />
           <span className="text-lg font-semibold text-white">
             Tự động bật tắt đèn khi môi trường sáng thay đổi
